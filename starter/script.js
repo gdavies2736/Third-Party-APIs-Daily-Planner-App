56b298a05cd9ba2a79205dd0da1363908b856b65
GazVanc <gdavies2736@gmail.com>
2023-11-25 12:37:38 +0000

    started code for today's date

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,15 @@
+//Display date and time at start
+//Need to run function to call and display date on page load
+
+var currentDay = moment().format('LL');
+$("#currentDay").text(currentDay);
+
+console.log(currentDay);
+
+
+
+
+
 $(".time-block").each(function(){
 var currentHour = dayjs().hour()
 var blockHour = parseInt($(this).attr("id").split("-")[1])
