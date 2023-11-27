6b0609d89d72bcd4402eebfb4d25b032d61d83ae
GazVanc <gdavies2736@gmail.com>
2023-11-27 19:02:16 +0000

    added event listener to button, set item to local storage and retrieved items from local storage using get item.

--- starter/script.js
+++ starter/script.js
@@ -1,14 +1,19 @@
-//Display date and time at start
-//Need to run function to call and display date on page load
+//Display current days date using moment.js.
+//Make sure date displayed Month, date, year
+//example November 20, 2023
 
 var currentDay = moment().format('LL');
 $("#currentDay").text(currentDay);
 
-console.log(currentDay);
 
-
-
-
+//Added timeblocks to HTML file
+//get time-block element
+//create variables for currentHour
+//create variable for each block hour
+//colour code past, present, future using if/else statement
+//if blockHour < currentHour = past (if)
+//if blockHour === currentHour = present (else if)
+//if blockHour < currentHour = future (else)
 
 $(".time-block").each(function(){
 var currentHour = dayjs().hour()
@@ -21,4 +26,29 @@ if (blockHour < currentHour) {
 else {
     $(this).addClass("future")   
 }
+});
+
+
+//Save button event listener
+//Save data to local storage
+
+$(".saveBtn").on("click", function(event){
+var value;
+var time;
+if (event.target.nodeName === "BUTTON") {
+    value = $(event.target).siblings(".description").val();
+    time = $(event.target).parent().attr("id"); 
+} else {
+   value = $(event.target).parent().siblings(".description").val()
+   time = $(event.target).parent().parent().attr("id");
+}
+/*var value = $(event.target).siblings(".description").val();
+var time = $(event.target).parent().attr("id");*/
+
+localStorage.setItem(time, value);
 })
+
+for (let i = 9; i < 18; i++) {
+  $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+    
+}
